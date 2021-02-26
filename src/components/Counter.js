199d34ee5e90574b67d22ab6df79c64099478f5c
jwilson42 <wilson.jd022@gmail.com>
2021-02-25 16:19:53 -0800

    added basic functional component with differing hooks relating different lifestyle methods

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = (props) => {
+  const [count, setCount] = useState(props.initialNumber);
+
+  const updateByFive = () => {
+    setCount(count + 5);
+  }
+
+  const subtractByFive = () => {
+    setCount(count - 5);
+  }
+
+  useEffect(() => {
+    console.log('I mounted or uptdated');
+  });
+
+  useEffect(() => {
+    console.log('I mounted');
+  }, []);
+
+  useEffect(() => {
+    console.log('count updated');
+  }, [count]);
+
   return (
     <>
-      Hi
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={updateByFive}>Add</button>
+      <button onClick={subtractByFive}>Subtract</button>
     </>
   )
 }
